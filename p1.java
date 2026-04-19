371617e7da2c37629a873bfccd51965cf0d860ce
Chaithra Shree <chaithrashree2812@gmail.com>
2026-04-19 22:04:03 +0545

    Update p1.java

--- p1.java
+++ p1.java
@@ -1,3 +1,5 @@
+/*1. Write a java program for getting different colors through ArrayList interface and searchwhether
+the color "Red" is available or not*/
 package ArrayList;
 import java.util.*;
 public class p1 {
